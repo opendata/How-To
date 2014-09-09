371580a82b0e86737db3fd74ed0e5cb8c39c7f26
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-09-09 17:04:54 +0200

    header permalink appearances; remove fontAwesome; source FA from cdn

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(function() {
+  return $("h1, h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="fa fa-link"></i>';
+    if (id) {
+      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
